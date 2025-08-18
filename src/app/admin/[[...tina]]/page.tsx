e3066a406c8f13b0a15bf2ce441211c080e61a3e
incrediblepanda <justin.bosco@outlook.com>
2025-08-18 00:28:06 -0500

    Update page.tsx

--- src/app/admin/[[...tina]]/page.tsx
+++ src/app/admin/[[...tina]]/page.tsx
@@ -1,7 +1,8 @@
 'use client';
+
 import { TinaAdmin } from 'tinacms';
+import tinaConfig from '../../../../tina/config';
 
 export default function AdminPage() {
-  return <TinaAdmin />;
+  return <TinaAdmin config={tinaConfig} />;
 }
-
